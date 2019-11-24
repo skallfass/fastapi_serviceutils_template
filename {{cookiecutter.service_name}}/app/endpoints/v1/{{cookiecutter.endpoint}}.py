07d6a6f2d6b72a7e13689fca8f6e46a17a86479b
Simon Kallfass <simon.kallfass@gmx.net>
2019-11-24 11:54:47 +0100

    added basic docstring for the endpoint

--- {{cookiecutter.service_name}}/app/endpoints/v1/{{cookiecutter.endpoint}}.py
+++ {{cookiecutter.service_name}}/app/endpoints/v1/{{cookiecutter.endpoint}}.py
@@ -5,8 +5,8 @@ from fastapi_serviceutils.app import create_id_logger
 from fastapi_serviceutils.app import Endpoint
 from starlette.requests import Request
 
-from app.endpoints.v1.models import Example as Output
-from app.endpoints.v1.models import GetExample as Input
+from app.endpoints.v1.models import Example
+from app.endpoints.v1.models import GetExample
 
 
 ENDPOINT = Endpoint(router=APIRouter(), route="/{{cookiecutter.endpoint}}", version="v1")
@@ -14,8 +14,19 @@ SUMMARY = "Example requests."
 EXAMPLE = Body(..., example={"msg": "some message."})
 
 
-@ENDPOINT.router.post("/", response_model=Output, summary=SUMMARY)
-async def {{cookiecutter.endpoint}}(request: Request, params: Input = EXAMPLE) -> Output:
+@ENDPOINT.router.post("/", response_model=Example, summary=SUMMARY)
+async def {{cookiecutter.endpoint}}(request: Request, params: GetExample = EXAMPLE) -> Example:
+    """Example docstring.
+
+    #Parameters:
+        request (Request): the original request.
+        params (Example): the request-parameters converted to an instance of
+            `Example`.
+
+    # Returns:
+        (GetExample) the result for the request.
+
+    """
     _, log = create_id_logger(request=request, endpoint=ENDPOINT)
     log.debug(f"received request for {request.url} with params {params}.")
-    return Output(msg=params.msg)
+    return Example(msg=params.msg)
